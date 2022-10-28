bcf10afa107dae78fcd454a4d4635ae8bca231d6
tk-yoshimura <yoshimura8192@gmail.com>
2022-10-28 13:19:34 +0900

    + forward

--- FiniteDifferenceFormula/Program.cs
+++ FiniteDifferenceFormula/Program.cs
@@ -5,44 +5,43 @@ using System.Linq;
 
 namespace FiniteDifferenceFormula {
     internal class Program {
-        static void Main(string[] args) {
+        static void Main() {
             const int degree = 64;
             
             List<int> vs = new() { 0 };
 
-            for (int i = 1; i <= degree; i++) {
+            for (int i = 1; i < degree * 2; i++) {
                 vs.Add(i);
-                vs.Add(-i);
             }
 
             Dictionary<(int degree, int accuracy), Fraction[]> table1 = Coef.Generate(
                 max_degree: degree, x0: 0, vs.Select((v) => new Fraction(v)).ToArray()
             );
 
-            using (StreamWriter sw = new StreamWriter($"../../../../results/centered_intway_acc{degree}.md")) {
+            using (StreamWriter sw = new StreamWriter($"../../../../results/forward_intway_acc{degree}.md")) {
                 sw.Write("|derivative|accuracy|");
-                for (int i = 0; i <= degree; i++) {
+                for (int i = 0; i < degree * 2; i++) {
                     sw.Write($"{i}|");
                 }
                 sw.Write("\n|----|----|");
-                for (int i = 0; i <= degree; i++) {
+                for (int i = 0; i < degree * 2; i++) {
                     sw.Write("----|");
                 }
                 sw.Write("\n");
 
                 foreach (Dictionary<(int degree, int accuracy), Fraction[]> table in new[] { table1 }) {
                     foreach (var key in table.Keys) {
-                        if (key.accuracy != degree) {
+                        if (key.accuracy != degree + ((~key.degree) & 1)) {
                             continue;
                         }
 
-                        sw.Write($"|{key.degree}|{key.accuracy}|");
+                        sw.Write($"|{key.degree}|{degree}|");
 
                         Fraction[] fs = table[key];
 
                         Array.Sort(vs.ToArray(), fs);
 
-                        foreach (var f in fs.Skip(fs.Length / 2)) {
+                        foreach (var f in fs) {
                             sw.Write($"{f}|");
                         }
 
